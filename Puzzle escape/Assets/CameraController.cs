06ca81f1854e073774de38408e95dc3306ea4e84
Mohamed Mohamed <mohamed.mohamed5@mail.mcgill.ca>
2020-01-07 10:41:38 -0500

    This is where i started my first commit.
    Added the map and the camera and how to move for the main character(not animated).

---
+++ Puzzle escape/Assets/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float panSpeed = 20f;
+    public float panBoarderThickness = 10f;
+    public Vector2 panLimit;
+    public Vector2 panLimit2;
+    public float scrollSpeed = 20f;
+    public float minY=20f;
+    public float maxY = 12000f;
+
+    public float posx;
+    public float posy;
+    public float posz;
+
+    void Start()
+    {
+        transform.position = transform.position;   
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Input.GetKey("w") || (Input.mousePosition.y >= Screen.height - panBoarderThickness))
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || (Input.mousePosition.y <= panBoarderThickness))
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || (Input.mousePosition.x >= Screen.width - panBoarderThickness))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || (Input.mousePosition.x <= panBoarderThickness))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+        
+        pos.x = Mathf.Clamp(pos.x, -panLimit2.x, panLimit.x);
+        pos.z= Mathf.Clamp(pos.z, -panLimit2.y, panLimit.y);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        
+        transform.position = pos;
+        posx = transform.position.x;
+        posy = transform.position.y;
+        posz = transform.position.z;
+}
+}
---
+++ Puzzle escape/Assets/MouseManger.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class MouseManger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject hoveredObject;
+    public GameObject selectedObject;
+    public PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray =Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+
+        if(Physics.Raycast(ray, out hitInfo))//hitInfo takes the object hit
+        {
+            GameObject hitObject = hitInfo.transform.root.gameObject;
+          
+            HoverObject(hitObject);
+
+        }
+        else
+        {
+            clearSelection();
+        }
+
+        if (Input.GetMouseButtonDown(0))//0 is left 1 is right 2 middle button down
+        {
+            if ((selectedObject == null) && (hoveredObject.CompareTag("Player")))
+            {
+                selectedObject = hoveredObject;
+               
+            }
+            if(selectedObject!=null)
+            {
+                SelectObject(hoveredObject, hitInfo);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))//0 is left 1 is right 2 middle button down
+        {
+            if (selectedObject != null)
+            {
+                selectedObject = null;
+            }
+        }
+
+    }
+
+    void HoverObject(GameObject obj)
+    {
+        if (hoveredObject != null)
+        {
+            if (obj == hoveredObject)
+                return;
+            clearSelection();
+        }
+        hoveredObject = obj;
+    }
+
+    void SelectObject(GameObject obj, RaycastHit hitInfo)
+    {
+        if (obj != null)
+        {
+            if (obj.CompareTag("Enemy"))
+            {
+                Debug.Log("attack " + obj);
+            }
+            else {
+
+                // selectedObject.GetComponent<NavMeshAgent>().SetDestination(obj.transform.position);
+                selectedObject.GetComponent<NavMeshAgent>().SetDestination(hitInfo.point);
+
+            }
+            
+        }
+    }
+
+    void clearSelection()
+    {
+        hoveredObject = null;
+    }
+}
